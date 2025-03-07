63f6cf5075845325ed7e58d9208debe41c8f3071
Benjamim Freitas <Ben.FreitasG@gmail.com>
2025-03-07 19:43:58 -0300

    Adiciona PORT ao .env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const productRoutes = require("./routes/productRoutes.js");
 const db = require("./db.js");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT ?? 8080;
 
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "pug");
